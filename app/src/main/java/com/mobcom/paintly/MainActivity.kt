f988959a2c5e140658e08d09a7642804f8654ece
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-10-08 18:52:14 +0700

    Implementasi Layout Galeri

--- app/src/main/java/com/mobcom/paintly/MainActivity.kt
+++ app/src/main/java/com/mobcom/paintly/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mobcom.paintly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_main)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    setContent("Home")
+                    true
+                }
+                R.id.menu_gallery -> {
+                    setContent("Notification")
+                    true
+                }
+                R.id.menu_profile -> {
+                    setContent("Search")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private fun setContent(content: String) {
+        setTitle(content)
+        tvLabel.text = content
     }
 }
