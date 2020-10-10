36ce6f5fb988e763fd2e41fa02ae1ea98bc49d91
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-10-10 12:19:15 +0700

    XML Layout Beranda/Home

--- app/src/main/java/com/mobcom/paintly/Home.kt
+++ app/src/main/java/com/mobcom/paintly/Home.kt
@@ -2,5 +2,5 @@ package com.mobcom.paintly
 
 import androidx.fragment.app.Fragment
 
-class Home : Fragment(R.layout.activity_gallery){   // activity_home
+class Home : Fragment(R.layout.activity_home){
 }
--- app/src/main/java/com/mobcom/paintly/Profile.kt
+++ app/src/main/java/com/mobcom/paintly/Profile.kt
@@ -2,5 +2,5 @@ package com.mobcom.paintly
 
 import androidx.fragment.app.Fragment
 
-class Profile : Fragment(R.layout.activity_gallery){    // activity_profile
+class Profile : Fragment(R.layout.activity_profile){
 }
