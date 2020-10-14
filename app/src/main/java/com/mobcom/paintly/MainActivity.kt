991027ac10bf4f8e67941704a8fbf8ece89d8133
Ervan Foster <ervanfoster@gmail.com>
2020-10-14 23:46:28 +0700

    Click Style Then Upload Pop up Implementation

--- app/src/main/java/com/mobcom/paintly/MainActivity.kt
+++ app/src/main/java/com/mobcom/paintly/MainActivity.kt
@@ -6,32 +6,20 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottomnav)
+        setContentView(R.layout.activity_home)
 
-        val homeFragment = Home()
-        val galleryFragment = Gallery()
-        val profileFragment = Profile()
+        val UploadSheetFragment = UploadSheetFragment()
 
-        setCurrentFragment(homeFragment)
-
-        bottomNav.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.menu_home -> setCurrentFragment(homeFragment)
-                R.id.menu_gallery -> setCurrentFragment(galleryFragment)
-                R.id.menu_profile -> setCurrentFragment(profileFragment)
-            }
-            true
+        btn_starryNight.setOnClickListener{
+            UploadSheetFragment.show(supportFragmentManager, "UploadSheetDialog")
         }
     }
 
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_fragment, fragment)
-            commit()
-        }
 }
