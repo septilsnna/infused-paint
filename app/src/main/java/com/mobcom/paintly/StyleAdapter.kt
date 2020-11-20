28826a250d8515a7b04db4485d2d815c4cbed577
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-11-21 03:55:44 +0700

    revisi sedikit

--- app/src/main/java/com/mobcom/paintly/StyleAdapter.kt
+++ app/src/main/java/com/mobcom/paintly/StyleAdapter.kt
@@ -10,56 +10,6 @@ import com.bumptech.glide.Glide
 import com.deeparteffects.sdk.android.model.Styles
 import com.mobcom.paintly.StyleAdapter.ViewHolder
 
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//import kotlinx.android.synthetic.main.style_item.view.*
-//
-//class StyleAdapter(private val styleList: ArrayList<StyleItem>, private val listener: OnItemClickListener) :
-//    RecyclerView.Adapter<StyleAdapter.StyleViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StyleViewHolder {
-//        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.style_item,
-//            parent, false)
-//
-//        return StyleViewHolder(itemView)
-//    }
-//
-//    override fun onBindViewHolder(holder: StyleViewHolder, position: Int) {
-//        val currentItem = styleList[position]
-//
-//        holder.imageView.setImageResource(currentItem.imageResource)
-//        holder.textView1.text = currentItem.text1
-//        holder.textView2.text = currentItem.text2
-//    }
-//
-//    override fun getItemCount() = styleList.size
-//
-//    inner class StyleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-//        val imageView: ImageView = itemView.btn_style
-//        val textView1: TextView = itemView.img_title
-//        val textView2: TextView = itemView.img_author
-//
-//        init {
-//            itemView.setOnClickListener(this)
-//        }
-//
-//        override fun onClick(v: View?) {
-//            val position = adapterPosition
-//            if (position != RecyclerView.NO_POSITION) {
-//                listener.onItemClick(position)
-//            }
-//        }
-//    }
-//
-//    interface OnItemClickListener {
-//        fun onItemClick(position: Int)
-//    }
-//}
-
 class StyleAdapter internal constructor(// StyleAdapter berbentuk RecyclerView.Adapter (langsung di set)
     private val mContext: Context,
     private val mStyles: Styles,
--- app/src/main/java/com/mobcom/paintly/StyleItem.kt
+++
@@ -1,3 +0,0 @@
-package com.mobcom.paintly
-
-data class StyleItem(val imageResource: Int, var text1: String, val text2: String)
