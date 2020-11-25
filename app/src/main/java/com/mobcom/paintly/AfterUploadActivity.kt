cbd68907d631b50d251ad4d2c60134638c8235f4
Ervan Foster <ervanfoster@gmail.com>
2020-11-25 23:36:22 +0700

    upload from camera fixing

--- app/src/main/java/com/mobcom/paintly/AfterUploadActivity.kt
+++ app/src/main/java/com/mobcom/paintly/AfterUploadActivity.kt
@@ -1,22 +1,27 @@
 package com.mobcom.paintly
 
+import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.mobcom.paintly.R.layout.afterchoosingimage
 import kotlinx.android.synthetic.main.afterchoosingimage.*
 import maes.tech.intentanim.CustomIntent
 import java.io.ByteArrayOutputStream
+import kotlin.math.roundToInt
 
 
 class AfterUploadActivity : AppCompatActivity() {
     var media: String? = null
     lateinit var imageBitmap: Bitmap
     var image_uri: Uri? = null
+    val REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +36,9 @@ class AfterUploadActivity : AppCompatActivity() {
             values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
             image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
             intent_action = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//            intent_action.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+
+        //intent_action = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //intent_action.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
         } else {
             intent_action = Intent(Intent.ACTION_GET_CONTENT)
             intent_action.type = "image/*"
@@ -56,20 +63,28 @@ class AfterUploadActivity : AppCompatActivity() {
         }
     }
 
+
+
+
     //gallery
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
         if (requestCode == 100) {
             if (resultCode == RESULT_OK) {
                 if (media == "CAMERA") {
-//                    val a = intent.getByteArrayExtra(MediaStore.EXTRA_OUTPUT)
-//                    imageBitmap = image_uri
-                    image_view.setImageURI(image_uri)
+                    if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+                        val img = data.extras?.get("data") as Bitmap
+                        val img2 = Bitmap.createScaledBitmap(img, (img.width*0.7).roundToInt(), (img.height*0.7).roundToInt(), false)
+                        Glide.with(this).load(img2).into(image_view)
+                    }
                 } else {
                     imageBitmap = ImageHelper.loadSizeLimitedBitmapFromUri(
                         data!!.data,
                         this.contentResolver, 768
                     )!!
-                    image_view.setImageBitmap(imageBitmap)
+                    val img = imageBitmap
+                    val img2 = Bitmap.createScaledBitmap(img, (img.width*0.7).roundToInt(), (img.height*0.7).roundToInt(), false)
+                    Glide.with(this).load(img2).into(image_view)
                 }
             } else {
                 finish()
