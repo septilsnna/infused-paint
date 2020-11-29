66bf2377653e4eca21abb078fc1c21f9030fc446
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-11-29 18:15:37 +0700

    Adding splash screen

---
+++ app/src/main/java/com/mobcom/paintly/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.mobcom.paintly
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import maes.tech.intentanim.CustomIntent
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed(Runnable {
+            try {
+                val intent = Intent(applicationContext, BottomNavActivity::class.java)
+                startActivity(intent)
+                finish()
+                CustomIntent.customType(this, "fadein-to-fadeout")
+            } catch (ignored: Exception) {
+                ignored.printStackTrace()
+            }
+        }, 250)
+    }
+}
